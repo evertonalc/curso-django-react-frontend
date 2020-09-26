3b6d6c885eaafc23b479c67db6b269a752b0ade1
Everton <evertonalc@hotmail.com>
2020-09-26 11:06:14 -0300

    Criando component List

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+/*import App from './App';*/
 import * as serviceWorker from './serviceWorker';
+import ListComponent from './ListComponent';
 
-function Test(){
-  return <h1>Hello World !</h1>
-}
 
 ReactDOM.render(
   <React.StrictMode>
-    <Test />
-    <App />
+   {/* <App /> */}
+    <ListComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
