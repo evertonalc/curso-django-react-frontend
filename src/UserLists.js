f2d4b1509cec0cc9d8ab0c935ebcee82e8689f79
Everton <evertonalc@hotmail.com>
2020-09-26 13:33:14 -0300

    Add Token ao aatributo headers para chamada com fetch

--- src/UserLists.js
+++ src/UserLists.js
@@ -5,8 +5,15 @@ export default class UserLists extends React.Component{
     state = { lists: null, loading: true }
 
     async componentDidMount(){
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        config.headers['Authorization'] = 'Token 5a763f7b7f27bd3011547e2d95b3dfbefe8afa59';
+
         var url = 'http://127.0.0.1:8000/list/';
-        const response = await fetch(url);
+        const response = await fetch(url, config);
         const data = await response.json();
         console.log(data);
         this.setState({lists: data, loading: false});
