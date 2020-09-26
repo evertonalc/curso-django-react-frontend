1f8415b011974c9c1c15aaabea7c3e91baf4fd91
Everton <evertonalc@hotmail.com>
2020-09-26 11:23:01 -0300

    criação do componente Item

---
+++ src/ItemComponent.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+
+export default function ItemComponent(props){
+    return <li>Item Desc.: {props.name}</li>
+}
--- src/ListComponent.js
+++ src/ListComponent.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import ItemComponent from './ItemComponent';
 export default function ListComponent(){
     return(
          <div> 
             <h2>Minha Lista</h2>
             <ul>
-                <li>Item 1</li>
-                <li>Item 2</li>
+                <ItemComponent name={'Meu Item'} />
+                <ItemComponent name={'Meu Item 2'} />
              </ul>
          </div> 
     )
